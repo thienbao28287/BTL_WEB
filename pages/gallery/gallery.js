e639e23365a533012b845473a362c28e2170d0da
= <=>
2026-02-10 15:27:28 +0700

    Add dropdown menu and update gallery page

--- pages/gallery/gallery.js
+++ pages/gallery/gallery.js
@@ -1,9 +1,18 @@
+//=====================SLIDER====================================
 const swiper = new Swiper('.swiper', {
+    effect: 'coverflow',
     // Cấu hình số lượng ảnh hiển thị
-    slidesPerView: 3,
-    spaceBetween: 20,
+    slidesPerView: 'auto',
+    centeredSlides: true,
     loop: true,
-    
+    // Cấu hình chi tiết cho hiệu ứng coverflow
+    coverflowEffect: {
+        rotate: 50,         // Độ xoay của các slide 2 bên
+        stretch: 0,         // Khoảng cách giãn ra giữa các slide
+        depth: 100,         // Độ sâu (trục Z), tạo hiệu ứng 3D
+        modifier: 1,        // Hệ số nhân của hiệu ứng
+        slideShadows: true, // Bật bóng đổ cho các slide 2 bên
+      },
     // Tự động chạy
     autoplay: {
         delay: 3000,
@@ -16,13 +25,13 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 
-    // Kết nối nút bấm tùy chỉnh của bạn
+    // Kết nối nút bấm tùy chỉnh
     navigation: {
         nextEl: '#next',
         prevEl: '#pre',
     },
 
-    // Responsive: Hiển thị nhiều ảnh hơn trên màn hình lớn
+    // Responsive
     breakpoints: {
         640: {
             slidesPerView: 2,
@@ -31,4 +40,28 @@ const swiper = new Swiper('.swiper', {
             slidesPerView: 3,
         }
     }
+});
+
+
+//=====================FILTER====================================
+const buttons = document.querySelectorAll(".filter-buttons button");
+const items = document.querySelectorAll(".gallery-grid .item");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        buttons.forEach(btn => btn.classList.remove("active"));
+        button.classList.add("active");
+
+        const filter = button.innerText.toLowerCase();
+
+        items.forEach(item => {
+            const type = item.dataset.type;
+
+            if (filter === "all" || type === filter) {
+                item.classList.remove("hide");
+            } else {
+                item.classList.add("hide");
+            }
+        });
+    });
 });

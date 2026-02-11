e59cfeb8c7168beb9d7d7b89b55110b3fc0e7279
= <=>
2026-02-11 13:45:00 +0700

    Update faq page

--- pages/faq/faq.js
+++ pages/faq/faq.js
@@ -4,7 +4,7 @@ const items = document.querySelectorAll(".faq-item")
 //===========================FILTER===================================
 buttons.forEach(button => {
     button.addEventListener("click", () => {
-        buttons.forEach(btn, () => btn.classList.remove("active"));
+        buttons.forEach(btn => btn.classList.remove("active"));
         button.classList.add("active");
 
         const filter = button.dataset.filter;
@@ -26,8 +26,16 @@ buttons.forEach(button => {
 //===========================TOGGLE FAQ===================================
 items.forEach(item => {
     const question = item.querySelector(".question"); //lấy phần tử có class question  bên trong item
+    const answer = item.querySelector(".answer");
+
     question.addEventListener("click", () => {
         item.classList.toggle("active");
+
+        if (item.classList.contains("active")) {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        } else {
+            answer.style.maxHeight = null;
+        };
     });
 });
 
